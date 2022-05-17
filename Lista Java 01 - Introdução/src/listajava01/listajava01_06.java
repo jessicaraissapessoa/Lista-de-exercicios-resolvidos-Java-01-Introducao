51107dedc455a9f9686dcee5cc3061c45cc19090
Jéssica Raissa Pessoa <jessicaraissapessoa@gmail.com>
2022-05-17 17:26:59 -0300

    Update listajava01_06.java

--- Lista Java 01 - Introdução/src/listajava01/listajava01_06.java
+++ Lista Java 01 - Introdução/src/listajava01/listajava01_06.java
@@ -7,16 +7,16 @@ public class listajava01_06 {
 
 	public static void main(String[] args) {
 
-//Lista de ExercÌcios de Java 01 - IntroduÁ„o / B·sico
-//Professor: Junio Figueiredo
-//Aluna: JÈssica Raissa
+//Lista de Exerc√≠cios de Java 01 - Introdu√ß√£o / B√°sico
+//Organizador / Professor: Junio Figueiredo
+//Revisora / Diagramadora / Aluna: J√©ssica Raissa Pessoa
 		
 Scanner teclado = new Scanner (System.in);
 				
-/* 06. TrÍs amigos (Jo„o, Carlos e Paulo), decidiram rachar igualmente uma conta de um bar.
- * FaÁa um programa para ler o valor total da conta e imprimir quanto cada um deve pagar.
- * Jo„o e Carlos n„o devem pagar os centavos.
- * Ex: Valor da conta R$9,70; Jo„o e Carlos pagam R$3,00, cada; Paulo paga R$3,70.
+/* 06. Tr√™s amigos (Jo√£o, Carlos e Paulo), decidiram rachar igualmente uma conta de um bar.
+ * Fa√ßa um programa para ler o valor total da conta e imprimir quanto cada um deve pagar.
+ * Jo√£o e Carlos n√£o devem pagar os centavos.
+ * Ex: Valor da conta R$9,70; Jo√£o e Carlos pagam R$3,00, cada; Paulo paga R$3,70.
  * Utilizar a classe a seguir:
  * DecimalFormat df = new DecimalFormat("##0.00");
  * df.format();*/
@@ -35,10 +35,10 @@ Scanner teclado = new Scanner (System.in);
 	double carlos = joao;
 	double paulo = conta - (joao + carlos);
 	
-	System.out.println("O valor total da conta È: " + reais.format(conta));
-	System.out.println("Jo„o dever· pagar: " + reais.format(joao));
-	System.out.println("Carlos dever· pagar: " + reais.format(carlos));
-	System.out.println("Paulo dever· pagar: " + reais.format(paulo));
+	System.out.println("O valor total da conta √©: " + reais.format(conta));
+	System.out.println("Jo√£o dever√° pagar: " + reais.format(joao));
+	System.out.println("Carlos dever√° pagar: " + reais.format(carlos));
+	System.out.println("Paulo dever√° pagar: " + reais.format(paulo));
 	
 	
 	//forma 2:
@@ -47,10 +47,10 @@ Scanner teclado = new Scanner (System.in);
 	int intterco = intconta / 3;
 	double comcentavos = conta - (2 * intterco);
 	
-	System.out.println("O valor total da conta È: " + reais.format(conta));
-	System.out.println("Jo„o dever· pagar: " + reais.format(intterco));
-	System.out.println("Carlos dever· pagar: " + reais.format(intterco));
-	System.out.println("Paulo dever· pagar: " + reais.format(comcentavos));
+	System.out.println("O valor total da conta √©: " + reais.format(conta));
+	System.out.println("Jo√£o dever√° pagar: " + reais.format(intterco));
+	System.out.println("Carlos dever√° pagar: " + reais.format(intterco));
+	System.out.println("Paulo dever√° pagar: " + reais.format(comcentavos));
 	
 teclado.close();
 
