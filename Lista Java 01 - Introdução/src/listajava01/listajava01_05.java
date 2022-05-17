b2e4fa964d1a4ded519bd969fe34e725c2d69f92
Jéssica Raissa Pessoa <jessicaraissapessoa@gmail.com>
2022-05-17 17:26:25 -0300

    Update listajava01_05.java

--- Lista Java 01 - Introdução/src/listajava01/listajava01_05.java
+++ Lista Java 01 - Introdução/src/listajava01/listajava01_05.java
@@ -7,21 +7,21 @@ public class listajava01_05 {
 
 	public static void main(String[] args) {
 
-//Lista de ExercÌcios de Java 01 - IntroduÁ„o / B·sico
-//Professor: Junio Figueiredo
-//Aluna: JÈssica Raissa
+//Lista de Exerc√≠cios de Java 01 - Introdu√ß√£o / B√°sico
+//Organizador / Professor: Junio Figueiredo
+//Revisora / Diagramadora / Aluna: J√©ssica Raissa Pessoa
 		
 Scanner teclado = new Scanner (System.in);
 				
-/* 05. A padaria da FPB vende certa quantidade de p„es franceses e certa quantidade de broas a cada dia.
- * Cada p„ozinho custa R$0.12 e cada broa custa R$1.50.
- * Ao final do dia, o dono quer saber o quanto arrecadou com as vendas de p„es e broas.
- * Ele deve, tambÈm, guardar 10% do que arrecadou na poupanÁa.
- * Desenvolva um programa para informar ao dono o valor arrecadado e o valor a ser depositado na poupanÁa. */
+/* 05. A padaria da FPB vende certa quantidade de p√£es franceses e certa quantidade de broas a cada dia.
+ * Cada p√£ozinho custa R$0.12 e cada broa custa R$1.50.
+ * Ao final do dia, o dono quer saber o quanto arrecadou com as vendas de p√£es e broas.
+ * Ele deve, tamb√©m, guardar 10% do que arrecadou na poupan√ßa.
+ * Desenvolva um programa para informar ao dono o valor arrecadado e o valor a ser depositado na poupan√ßa. */
 	
 	DecimalFormat reais = new DecimalFormat ("R$ ##0.00");
 	
-	System.out.println("Quantos p„ezinhos foram vendidos hoje?");
+	System.out.println("Quantos p√£ezinhos foram vendidos hoje?");
 	int qtddpaes = teclado.nextInt();
 	
 	System.out.println("Quantas broas foram vendidas hoje?");
@@ -35,7 +35,7 @@ Scanner teclado = new Scanner (System.in);
 	double poupanca = arrecadado * 0.1;
 	
 	System.out.println("O valor total arrecadado hoje foi: " + reais.format(arrecadado));
-	System.out.println("O valor a ser depositado na poupanÁa hoje È: " + reais.format(poupanca));
+	System.out.println("O valor a ser depositado na poupan√ßa hoje √©: " + reais.format(poupanca));
 
 teclado.close();
 
